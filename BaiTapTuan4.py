5a7d2f7af8ac2f99c50174797e917e0eb2fbc2b8
aurora <le8940741@gmail.com>
2025-11-27 21:58:38 +0700

    update hw

--- BaiTapTuan4.py
+++ BaiTapTuan4.py
@@ -1,5 +1,7 @@
 #BaiTapTuan4
 
+
+
 ###W4A1
 i = int(input("Enter an integer not higher than 1000: "))
 
@@ -140,6 +142,100 @@ if '42' in array:
     print("I have found the meaning of life!")
 else:
     print("Just a joke.")
+
+
+
+
+
+
+
+#W4A6
+
+
+
+from math import sqrt
+
+print("Tinh tong cac so nguyen to giua hai so nguyen duong nhap vao, so dau be hon so sau.")
+
+hai_so = input("Nhap hai so cach nhau boi dau cach: ").split()
+
+so_luong_so = len(hai_so)
+
+'''kiem tra xem user co nhap dung hai so khong, co chen them chu vao khong, nhap sai so luong khong,
+co dung so be truoc so lon khong'''
+
+while True:
+    check_result = 1   #1 la dung, 0 la sai
+
+#kiem tra dinh dang so
+    for run_check_1 in hai_so:
+        if run_check_1.isdigit(): 
+            pass
+
+            #kiem tra thu tu so be so lon
+            if run_check_1 == hai_so[so_luong_so - 1]:
+                if 0 < int(hai_so[0]) < int(hai_so[so_luong_so -1]): 
+                    #can phai de hai_so[so_luong_so -1] de phong truong hop chi 1 so duoc nhap vao
+                    #se loi chuong trinh khong chay tiep duoc vi khong co so thu hai
+                    #de la [so_luong_so -1] thi no se don gian la khien cho dieu kien bi sai
+                    #do 1 so khong the be hon chinh no
+                    #ket qua la neu chi nhap vao 1 so thi se khong duoc chap nhan, phai nhap lai
+                    pass
+                else:
+                    check_result = 0
+
+        else:
+            check_result = 0
+            break
+
+    if so_luong_so != 2 or check_result == 0:
+ 
+        hai_so = input("Sai so luong/dinh dang so/thu tu so be so lon, hay nhap lai: ").split() 
+        #thieu split dong tren la sai, phai dung kieu tap hop ban dau, fix mai moi nhin ra, lau v, mat ca buoi toi
+        so_luong_so = len(hai_so)
+    else: 
+        break
+
+'''tinh tong cac so nguyen to giua hai so be so lon'''
+
+#lap ham kiem tra so nguyen to
+
+from math import sqrt
+
+def kiem_tra_so_nguyen_to(r):
+    can_bac_2 = int(sqrt(r))
+
+    if r > 2 :
+        for i in range (2, can_bac_2 + 1): #mac loi sai khong cong them 1 vao can_bac_2, the la chuong
+                                           #trinh chi dem den can_bac_2 -1
+            if r%i == 0:
+                return 0
+        return r
+    elif r == 2:
+        return 2
+
+    else: #so be hon 2 thi khong phai so nguyen to
+        return 0
+
+#tong cac so nguyen to trong doan [so be, so lon]
+total = 0
+for count in range(int(hai_so[0]), int(hai_so[1]) + 1): #do trong tap hop nay la string, can chuyen ve int
+    #print(kiem_tra_so_nguyen_to(count)) - lenh nay dung de kiem tra loi trong chuong trinh
+    total += kiem_tra_so_nguyen_to(count)
+
+print(f"Tong cac so nguyen to giua hai so la: {total}")
+
+
+
+
+    
+
+
+
+    
+
+
+
 
     
 
